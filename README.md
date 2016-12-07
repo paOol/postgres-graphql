37056dfeab63c4561c7520cfa9fe9ce69c561863
paoolwindows <paool@windows.desktop>
2016-12-06 22:55:42 -0800

    initial commit
    new repo under organization. credentials moved due to public repo

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-postgres-graphql
+# postgres-testing
+migrations with version control
+
+# goals
+*be able to import a sample database with plenty of testing data
+
+*experiment with `GraphQL`
+
+*be able to sync local dbs with eachother, as well as sync with remote db through migrations
+
+*move credentials outside of repo into ENV variables. (more secure)
+
+-initial intro run might go something like this
+
+simple table created
+
+paul adds a field, saves 1st migration, then commits.
+
+rj runs migration (to have the new field).
+
+rj creates new field, saves 2nd migration, then commits.
+
+paul pulls latest changes, runs migrations.
+
+DBs are in sync with hopefully no conflicts.
+
+
+
+second run we can maybe mess with entries and more complex usage.
+
+third run, experiment with `git` strategies. Forking/pull requests.
---
+++ index.js
@@ -0,0 +1,47 @@
+const config = require('./config/config');
+const winston = require('winston');
+const Sequelize = require('sequelize');
+
+winston.configure({
+
+   transports: [
+     new (winston.transports.Console)({ level: 'debug' }),
+     new (winston.transports.File)({ filename: 'logs/postgresIndexjs.log' , level: 'info' })
+   ]
+
+   // move this to ./config/config eventually
+});
+
+let sequelize = new Sequelize(`postgres://${config.database.username}:${config.database.password}@${config.database.host}:${config.database.port}/${config.database.dbname}`);
+
+
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    winston.log('debug', 'Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    winston.log('error', 'Unable to connect to the database:');
+  });
+
+
+var User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+
+sequelize.sync().then(function() {
+  return User.create({
+    username: 'paul doe',
+    birthday: new Date(1989, 1, 12)
+  });
+}).then(function(jane) {
+
+  console.log(jane.get({
+    plain: true
+  }));
+});
+
+
