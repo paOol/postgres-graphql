d7e446f355beaf7c0ad84fc7bebe9c106cbea4eb
paul <paoolkim@gmail.com>
2016-12-08 08:23:03 -0800

    removed sequelize
    added knexfile to version control. using config.json

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
 const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
+const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'development']);
 const winston = require('winston');
-const Sequelize = require('sequelize');
 
 winston.configure({
-
-   transports: [
-     new (winston.transports.Console)({ level: 'debug', json: true }),
-     new (winston.transports.File)({ filename: 'logs/postgresIndexjs.log' , level: 'info' })
-   ]
-
-   // move this to ./config/config eventually
+  transports: [
+    new(winston.transports.Console)({ level: 'debug', json: true }),
+    new(winston.transports.File)({ filename: 'logs/postgresIndexjs.log', level: 'info' })
+  ]
+  // move this to ./config/config eventually
 });
 
-
-winston.log('debug', process.env);
-
-let sequelize = new Sequelize(`${config.POSTGRES_HOST}:${config.POSTGRES_PORT}/${config.POSTGRES_DBNAME}`);
-
-
-sequelize
-  .authenticate()
-  .then(function(err) {
-    winston.log('debug', 'Connection has been established successfully.');
+knex.migrate.latest()
+  .then(function() {
+    // run knex seed files if needed
+    //return knex.seed.run();
   })
-  .catch(function (err) {
-    winston.log('error', 'Unable to connect to the database:');
-  });
-
-
-// winston.log('debug', process.env);
-
-// var User = sequelize.define('user', {
-//   username: Sequelize.STRING,
-//   birthday: Sequelize.DATE
-// });
-
-
-// sequelize.sync().then(function() {
-//   return User.create({
-//     username: 'paul doe',
-//     birthday: new Date(1989, 1, 12)
-//   });
-// }).then(function(jane) {
-
-//   console.log(jane.get({
-//     plain: true
-//   }));
-// });
-
+  .then(function() {
+    winston.log('info', 'ran migrations from index.js. DB should be up to date.');
+});
