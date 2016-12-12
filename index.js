13ec8ea1152eca44c307e3fa1d185242aa0e27ec
paul <paoolkim@gmail.com>
2016-12-12 08:01:27 -0800

    postgraph as koa

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ let config = require('./config/config.json')[env];
 require('winston-papertrail').Papertrail;
 require('pretty-error').start();
 
-import postgraphql from 'postgraphql'
+import postgraphql from 'postgraphql';
 import Koa from 'koa';
 let app = new Koa();
 
 
-
+// move to koa middleware
 winston.configure({
   transports: [
       new(winston.transports.Console)({
@@ -25,7 +25,6 @@ winston.configure({
         level: 'info'
       })
     ]
-    // move this to ./config/config eventually
 });
 if (env == 'production') {
   winston.add(winston.transports.Papertrail, {
@@ -38,54 +37,45 @@ if (env == 'production') {
 }
 
 
-// import http from 'http'
-
-// http.createServer(postgraphql(
-//   'postgres:postgres:pw@localhost:5432/postgres',
-//   'public',
-//   {graphiql: true}
-//   )).listen(4000)
 
 app.use(
   postgraphql(
-    'postgres:postgres:pw@localhost:5432/postgres',
-    'public',
-    {graphiql: true}
+    `postgres:${config.USER}:${config.PASSWORD}@${config.HOST}:${config.PORT}/${config.DBNAME}`, //concat to shorten later
+    'public', //schema
+    { graphiql: true } //options
   )
-
 );
 
-  winston.log('debug', 'PostGraphQL connection established');
-  // await next();
+winston.log('debug', 'PostGraphQL connection established');
 
+
+// move this to middleware as well
+//
 //app.use(async (ctx, next) => {
-  //winston.log('debug', 'inside 2nd middleware');
-  // //eventually put this in a function and move out of index.js
-  // knex.migrate.latest()
-  //   .then(function() {
-  //     if (fs.existsSync(__dirname + `/db/seeds/${env}`)) {
-  //       winston.log('info', `seeding ${env} db`);
-  //       winston.log('info', 'ran migrations from index.js. DB should be up to date.');
-  //       return knex.seed.run();
-  //     } else {
-  //       winston.log('info', `${env} seed directory does not exist. seeding will not occur`);
-  //     }
-  //   })
-  //   .catch(function(err) {
-  //     winston.log('debug', err);
-  //     console.log(err);
-  //   })
-  //   .finally(function() {
-  //     process.exit();
-  //   });
+//winston.log('debug', 'inside 2nd middleware');
+// //eventually put this in a function and move out of index.js
+// knex.migrate.latest()
+//   .then(function() {
+//     if (fs.existsSync(__dirname + `/db/seeds/${env}`)) {
+//       winston.log('info', `seeding ${env} db`);
+//       winston.log('info', 'ran migrations from index.js. DB should be up to date.');
+//       return knex.seed.run();
+//     } else {
+//       winston.log('info', `${env} seed directory does not exist. seeding will not occur`);
+//     }
+//   })
+//   .catch(function(err) {
+//     winston.log('debug', err);
+//     console.log(err);
+//   })
+//   .finally(function() {
+//     process.exit();
+//   });
 
 
-  //await next();
+//await next();
 //});
 
 
 
 app.listen(3000, () => winston.log('debug', 'server running on localhost:3000'));
-
-
-
